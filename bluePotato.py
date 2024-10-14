c5de46761347abca49fb363c3472e6694a76f138
blue-w-potato <148214452+blue-w-potato@users.noreply.github.com>
2024-10-14 15:00:10 +0800

    Update bluePotato.py

--- bluePotato.py
+++ bluePotato.py
@@ -52,12 +52,41 @@ class append:
                 data[columnName] = np.ceil(average*t)/t
             return data 
         
-        def row(data:pd.DataFrame, f:int = 3, p:int = 1):
-            if f:
-                data = data._append({i:round(data[i].sum()/data[i].size, f) for i in data.columns}, ignore_index = True)
-            else:
-                data = data._append({i:data[i].sum()//data[i].size//(10**p)*(10**p) for i in data.columns}, ignore_index = True)
-            return data
+        def row(data:pd.DataFrame, f:int = 3, mode:int = 0):
+            '''
+            data: 來源資料，型別一定要DataFrame
+            rowName: 新列名
+            f: mode = 0 :不額外處理
+
+               mode = 1 :四捨五入到小數點後f位
+               mode = 2 :四捨五入到10**f 
+
+               mode = 3 :無條件捨去到小數點後f位
+               mode = 4 :無條件捨去到10**f 
+
+               mode = 5 :無條件進位到小數點後f位
+               mode = 6 :無條件進位到10**f 
+            '''
+            columns = pd.Series(data.columns)
+            t = (10**f)
+            if mode == 0:
+                average = {column:data[column].sum()/data[column].size for column in columns}
+                data = data._append(average, ignore_index = True)
+            if mode == 1:
+                average = {column:round(data[column].sum()/data[column].size,f) for column in columns}
+                data = data._append(average, ignore_index = True)
+            elif mode == 2:
+                average = {column:round(data[column].sum()/data[column].size/(10**(f-1)))*(10**(f-1)) for column in columns}
+                data = data._append(average, ignore_index = True)
+            
+            elif mode == 3:
+                f-=1
+                average = {column:np.ceil(data[column].sum()/data[column].size/(10**f))*(10**f) for column in columns}
+                data = data._append(average, ignore_index = True)
+            elif mode == 4:
+                average = {column:np.ceil(data[column].sum()/data[column].size*(10**f))/(10**f)  for column in columns}
+                data = data._append(average, ignore_index = True)
+            return data 
 
     
     
